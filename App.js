6c7fa089422855a3701b95d5b2f4d7da2b769534
dery <derydeviandi@gmail.com>
2019-11-18 11:37:35 +0700

    clone-gojek

--- App.js
+++ App.js
@@ -19,15 +19,25 @@ export default class App extends Component {
       //     <Text style={{ color: 'black' }}>Ini View 3</Text>
       //   </View>
       // </View>
-      <View style={{ flex: 1, flexDirection: 'column' }}>
-        <View style={{ height: 80, backgroundColor: 'grey', flexDirection: "row" }}>
-          <View style={{ flex: 3, backgroundColor: 'white' }}>
-            <TextInput placeholder="Looking for something?"
-              style={{ height: 50 }}
+      <View style={{ flex: 1, flexDirection: 'column', paddingVertical: 10, paddingHorizontal: 15  }}>
+        <View style={{ height: 80, flexDirection: "row"}}>
+          <View style={{ flex: 3 }}>
+            <Image
+              style={{ width: 30, height: 30, position: 'absolute', left: 10, top: 5 }}
               source={require('./Images/search.png')}
-            > </TextInput>
+            />
+            <TextInput placeholder="Looking for something?"
+              style={{ height: 40, borderWidth: 1, borderColor: '#E8E8E8', borderRadius: 50, paddingLeft: 50 }}
+            />
           </View>
-          <View style={{ flex: 1, backgroundColor: 'yellow' }}></View>
+
+          <View style={{ flex: 1, flexDirection: "row", alignItems: "center", height: 40, borderWidth:0.5, borderColor: '#E8E8E8', borderRadius: 50, paddingLeft: 10 }}>
+            <Image
+              style={{ width: 30, height: 30 }}
+              source={require('./Images/promo.png')}
+            />
+            <Text>Promo</Text>
+          </View>
         </View>
         <ScrollView>
           <Text>Ini Text</Text>
@@ -67,36 +77,36 @@ export default class App extends Component {
           <Text>Ini Text</Text>
           <Text>Ini Text</Text>
         </ScrollView>
-        <View style={{ height: 80, backgroundColor: 'white', flexDirection: "row", paddingHorizontal: 30, borderTopColor: '#E8E8E8', borderTopWidth: 5, paddingTop: 10 }}>
+        <View style={{ height: 80, backgroundColor: 'white', flexDirection: "row", paddingHorizontal: 30, borderTopColor: '#E8E8E8', borderTopWidth: 5, paddingVertical: 10 }}>
           <View style={{ flex: 1, alignItems: 'center' }}>
             <Image
               style={{ width: 30, height: 30, resizeMode: "contain" }}
               source={require('./Images/tab-beranda.png')} />
-            <Text>Home</Text>
+            <Text style={{ fontSize: 12 }}>Home</Text>
           </View>
           <View style={{ flex: 1, alignItems: 'center' }}>
             <Image
               style={{ width: 30, height: 30, resizeMode: "contain" }}
               source={require('./Images/tab-pesanan.png')} />
-            <Text>Orders</Text>
+            <Text style={{ fontSize: 12 }}>Orders</Text>
           </View>
           <View style={{ flex: 1, alignItems: 'center' }}>
             <Image
               style={{ width: 30, height: 30, resizeMode: "contain" }}
               source={require('./Images/tab-pesan.png')} />
-            <Text>Messages</Text>
+            <Text style={{ fontSize: 12 }}>Messages</Text>
           </View>
           <View style={{ flex: 1, alignItems: 'center' }}>
             <Image
               style={{ width: 30, height: 30, resizeMode: "contain" }}
               source={require('./Images/tab-inbox.png')} />
-            <Text>Inbox</Text>
+            <Text style={{ fontSize: 12 }}>Inbox</Text>
           </View>
           <View style={{ flex: 1, alignItems: 'center' }}>
             <Image
               style={{ width: 30, height: 30, resizeMode: "contain" }}
               source={require('./Images/tab-akun.png')} />
-            <Text>Account</Text>
+            <Text style={{ fontSize: 12 }}>Account</Text>
           </View>
         </View>
       </View>
